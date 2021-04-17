b8b4740240edfa1eb7763e744b9470ac1f670b4e
PB7P <PB7P@MI00273033.petrobras.biz>
2021-04-17 18:17:21 -0300

    Ordenação por Seleção - selectionsort

--- estrutura-de-dados-C/main.c
+++ estrutura-de-dados-C/main.c
@@ -8,18 +8,50 @@ void trocarNumeros(int &a, int &b)
     b = tmp;
 }
 
-void ordenacaoBolha(int * vetor, int size)
+void ordenacaoBolha(int * vetor, int tamanho)
 {
 
-    for (int i = 0 ; i < size - 1 ; i++)
+    for (int i = 0 ; i < tamanho - 1 ; i++)
     {
-        for (int j = i + 1; j < size; j++)
+        for (int j = i + 1; j < tamanho; j++)
         {
             if (vetor[i] > vetor[j])
             {
                 trocarNumeros(vetor[i], vetor[j]);
             }
         }
+
+    }
+
+}
+
+void selectionsort(int * vetor, int tamanho)
+{
+    int posicaoMenor;
+
+
+    for (int i = 0 ; i < tamanho; i++)
+    {
+        // posição do menor valor.
+        posicaoMenor = i;
+
+        for (int j = i+1; j < tamanho; j++)
+        {
+            if(vetor[posicaoMenor] > vetor[j])
+            {
+                //Pegando a menor posição
+                posicaoMenor = j;
+            }
+
+
+        }
+
+        // Para garatir que não haja comparações de valores iguais
+        if(i != posicaoMenor)
+        {
+            swap(vetor[posicaoMenor], vetor[i]);
+        }
+
 
     }
 
